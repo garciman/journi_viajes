d049fd859873fa0b9fc4e27e9693fbe3e050f20e
GuilleTorresPerez <guilletorresperez@gmail.com>
2025-10-29 20:07:37 +0100

    add flutter test coverage to gitignore

---
+++ journi/test/coverage_helper_test.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: unused_import
+import 'package:journi/application/entry_service.dart';
+import 'package:journi/application/shared/result.dart';
+import 'package:journi/application/trip_service.dart';
+import 'package:journi/application/use_cases/entry_use_cases.dart';
+import 'package:journi/application/use_cases/use_cases.dart';
+import 'package:journi/data/memory/in_memory_entry_repository.dart';
+import 'package:journi/data/memory/in_memory_trip_repository.dart';
+import 'package:journi/domain/entry.dart';
+import 'package:journi/domain/ports/entry_repository.dart';
+import 'package:journi/domain/ports/trip_repository.dart';
+import 'package:journi/domain/trip.dart';
+import 'package:journi/domain/trip_extensions.dart';
+import 'package:journi/domain/trip_queries.dart';
+// UI / screens en la raíz:
+import 'package:journi/crear_viaje.dart';
+import 'package:journi/editar_viaje.dart';
+import 'package:journi/example/entries_usage_example.dart';
+import 'package:journi/map_screen.dart';
+import 'package:journi/pantalla_viaje.dart';
+void main() {}
